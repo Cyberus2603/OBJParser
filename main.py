e0e3df298c28f2f6c2fd1ec68d4ffd1da65ca5ad
Maks <cyberus2603@gmail.com>
2020-04-23 19:42:58 +0200

    Fixed Color and Vertexes

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
     except IOError:
         print('File ', input_file_name, ' couldn\'t be opened')
         sys.exit(1)
+
     try:
         material_file = open(material_file_name, 'r')
     except IOError:
@@ -31,8 +32,11 @@ def main():
 
     result_names = []
     result_trv = []
-    trv_count = []
     result_txv = []
+    faces_tab = []
+    offset_value = 0
+    triangle_vertecies_with_faces = []
+    color_counter = []
 
     materials_obj = []
     materials_mtl = []
@@ -56,23 +60,43 @@ def main():
         if line.startswith('vn'):
             continue
 
+        if line.startswith('f'):
+            faces_tab[len(result_trv) - 1].append(splited_string[1] + ', ' + splited_string[2] + ', ' + splited_string[3] + ', \n')
+
         if line.startswith('usemtl'):
             materials_obj.append(splited_string[1])
 
         if line.startswith('o'):
             result_names.append('\n' + hashes + ' ' + splited_string[1] + ' ' + hashes + '\n \n')
-            trv_count.append(0)
-            result_trv.append('')
+            triangle_vertecies_with_faces.append('')
+            result_trv.append([])
             result_txv.append('')
+            faces_tab.append([])
+            color_counter.append(0)
 
         if line.startswith('v'):
             if len(splited_string) != 4:
                 print("Not triangle only .obj file")
                 sys.exit(2)
-            result_trv[len(result_trv) - 1] += splited_string[1] + ', '
-            result_trv[len(result_trv) - 1] += splited_string[2] + ', '
-            result_trv[len(result_trv) - 1] += splited_string[3] + ', \n'
-            trv_count[len(result_trv) - 1] += 1
+            result_trv[len(result_trv) - 1].append(splited_string[1] + ', ' + splited_string[2] + ', ' + splited_string[3] + ', \n')
+
+    for i in range(0, len(faces_tab)):
+        for j in range(0, len(faces_tab[i])):
+            line = faces_tab[i][j].rstrip(',')
+            splited_string = line.split(' ')
+            splited_vertex_id_1 = splited_string[0].split('/')
+            splited_vertex_id_2 = splited_string[1].split('/')
+            splited_vertex_id_3 = splited_string[2].split('/')
+            splited_vertex_id_1 = int(splited_vertex_id_1[0]) - offset_value
+            splited_vertex_id_2 = int(splited_vertex_id_2[0]) - offset_value
+            splited_vertex_id_3 = int(splited_vertex_id_3[0]) - offset_value
+            triangle_vertecies_with_faces[i] += (result_trv[i][splited_vertex_id_1 - 1]
+                                                 + result_trv[i][splited_vertex_id_2 - 1]
+                                                 + result_trv[i][splited_vertex_id_3 - 1])
+            color_counter[i] += 3
+        offset_value += len(result_trv[i])
+
+
 
     for line in material_file.readlines():
         line = line.rstrip()
@@ -103,7 +127,7 @@ def main():
         sys.exit(1)
 
     for i in range(0, len(result_names)):
-        output_triangle_file.write(result_names[i] + result_trv[i])
+        output_triangle_file.write(result_names[i] + triangle_vertecies_with_faces[i])
 
     for i in range(0, len(result_names)):
         output_textures_file.write(result_names[i] + result_txv[i])
@@ -112,7 +136,7 @@ def main():
         for j in range(len(materials_mtl)):
             if materials_obj[i] == materials_mtl[j]:
                 output_colors_file.write(result_names[j])
-                for k in range(0, trv_count[j]):
+                for k in range(0, color_counter[j]):
                     output_colors_file.write(colors_mtl[j])
 
     output_triangle_file.close()
